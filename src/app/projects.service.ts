d39c7e528f69fd8640c270b354c2a06966b8894d
Venkat varma <kvenkatvarma@gmail.com>
2025-03-04 18:30:26 +0530

    RxJS Map operator

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Project } from './project';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,15 @@ export class ProjectsService {
   constructor(private httpClient:HttpClient) { }
 
   getAllProjects():Observable<Project[]>{
-    return this.httpClient.get<Project[]>("/api/projects");
+    return this.httpClient.get<Project[]>("/api/projects").pipe(map(
+      (data:Project[])=>{
+        for(let i =0; i <data.length;i++)
+        {
+          data[i].teamSize = data[i].teamSize * 100;
+        }
+         return data;
+      }
+    ));
   }
 
   insertProject(newProject:Project):Observable<Project>{
