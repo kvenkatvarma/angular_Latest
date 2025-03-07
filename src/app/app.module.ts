1008ebd58ec6912c380793a1dfa955056635820e
Venkat varma <kvenkatvarma@gmail.com>
2025-03-07 17:06:45 +0530

    User Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     AdminModule
   ],
---
+++ src/app/login.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginViewModel } from './login-view-model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient:HttpClient) { }
+  currentUserName:any = null;
+
+  public login(loginViewModel:LoginViewModel):Observable<any>{
+   return this.httpClient.post<any>("/authenticate",loginViewModel).pipe(map(user=>{
+     if(user)
+     {
+
+      this.currentUserName = user.userName;
+     }
+     return user;
+   }));
+  }
+  public logout(){
+    this.currentUserName = null;
+  }
+}
