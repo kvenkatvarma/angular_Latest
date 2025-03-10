b3f1b9220676667de905ccc11730db4c45527605
Venkat varma <kvenkatvarma@gmail.com>
2025-03-10 16:30:20 +0530

    Validations in Reactive Forms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,FormArray,FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup,FormArray,FormBuilder, Validators } from '@angular/forms';
 import { CountriesService } from '../countries.service';
 import { Country } from '../country';
 
@@ -20,15 +20,15 @@ export class SignUpComponent implements OnInit {
 
     this.signUpForm = this.formBuilder.group({
       personName: this.formBuilder.group({
-        firstName:null,
-        lastName:null,
+        firstName:[null,[Validators.required,Validators.minLength(2)]],
+        lastName:[null,[Validators.required,Validators.minLength(2)]]
       }),
 
-     email:null,
-     mobile:null,
-     dateOfBirth:null,
-     gender :null,
-     countryID : null,
+      email: [null, [Validators.required, Validators.email]],
+      mobile: [null, [Validators.required, Validators.pattern(/^[789]\d{9}$/)]],
+      dateOfBirth: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+      countryID: [null, [Validators.required]],
      receiveNewsLetters:null,
      skills: this.formBuilder.array([])
     });
@@ -39,6 +39,7 @@ export class SignUpComponent implements OnInit {
     );
   }
   onSubmitClick(){
+    this.signUpForm["submitted"] = true;
     this.signUpForm.setValue({
     firstName : "Adam",
     lastName: "Varma",
