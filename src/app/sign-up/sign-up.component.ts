a092c39d4ab0594a69544c720c27a543a261e6d6
Venkat varma <kvenkatvarma@gmail.com>
2025-03-10 14:13:05 +0530

    dynamic dropdown

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { CountriesService } from '../countries.service';
+import { Country } from '../country';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,17 +12,20 @@ export class SignUpComponent implements OnInit {
  
   signUpForm: FormGroup;
   genders =["male","female"];
-  
-  constructor() { }
+  countries : Country[] = [];
+  constructor(private countryService:CountriesService) { }
 
   ngOnInit(): void {
+     this.countries = this.countryService.getCountries();
+
     this.signUpForm = new FormGroup({
      firstName:new FormControl(null),
      lastName:new FormControl(null),
      email:new FormControl(null),
      mobile:new FormControl(null),
      dateOfBirth:new FormControl(null),
-     gender :new FormControl(null)
+     gender :new FormControl(null),
+     countryID : new FormControl(null)
     });
   }
 
